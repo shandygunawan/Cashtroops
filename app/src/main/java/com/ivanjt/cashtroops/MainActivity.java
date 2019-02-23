6904e7ea3cb7e793d3b1872e299db402d1494c8b
Shandy <shandy.gunawan98@gmail.com>
2019-02-24 01:30:05 +0700

    layout main still in change

--- app/src/main/java/com/ivanjt/cashtroops/MainActivity.java
+++ app/src/main/java/com/ivanjt/cashtroops/MainActivity.java
@@ -21,6 +21,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.ivanjt.cashtroops.model.User;
+import com.ivanjt.cashtroops.model.Wallet;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
